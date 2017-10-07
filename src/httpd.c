a54ab17e6193595bab73c6b3735f860be4b7b3c8
brynjarb15 <brynjarb15@skel.ru.is>
2017-10-07 15:22:27 +0000

    komin tenging

--- src/httpd.c
+++ src/httpd.c
@@ -5,17 +5,31 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
 
-int main(int argc, char *argv[])
-{
-	int sockfd;
+int main(int argc, char *argv[]) {
+    
+    char* htmlCode = "HTTP/1.1 200 OK\n"
+    "Date: Mon, 27 Jul 2009 12:28:53 GMT\n"
+    "Server: Apache\n"
+    "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n"
+    "ETag: \"34aa387-d-1568eb00\"\n"
+    "Accept-Ranges: bytes\n"
+    "Content-Length: 51\n"
+    "Vary: Accept-Encoding\n"
+    "Content-Type: text/html\n"
+    "\n"
+    //"Hello World! My payload includes a trailing CRLF.\n";
+    "<!doctype html><body><h1>Page</h1></body></html>";
+    printf("start of the program \n");
+    int sockfd;
     struct sockaddr_in server, client;
-	char message[512];
+    char message[512];	
+    // Getting 
+    int port = strtol(argv[1], NULL, 10);
 	
-	// Getting 
-	int port = strtol(argv[1], NULL, 10);
-	
-	// Create and bind a TCP socket.
+    // Create and bind a TCP socket.
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     // Network functions need arguments in network byte order instead of
@@ -28,18 +42,32 @@ int main(int argc, char *argv[])
 	// Before the server can accept messages, it has to listen to the
     // welcome port. A backlog of one connection is allowed.
     listen(sockfd, 1);
-	
-	
-	for (;;) {
-		// We first have to accept a TCP connection, connfd is a fresh
+
+    int connfd;
+    for (;;) {
+	printf("begging of for loop\n");
+    	// We first have to accept a TCP connection, connfd is a fresh
         // handle dedicated to this connection.
         socklen_t len = (socklen_t) sizeof(client);
-        int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
-		
-		// Close the connection.
-		shutdown(connfd, SHUT_RDWR);
-        close(connfd);
-	}
-	
-	return 0;
+        connfd = accept(sockfd, (struct sockaddr *) &client, &len);
+	char *ipNumber = inet_ntoa(client.sin_addr);
+	int portNumberFromClient = ntohs(client.sin_port);
+	printf("ipNumber: %s:%d\n", ipNumber, portNumberFromClient);
+	ssize_t n = recv(connfd, message, sizeof(message) - 1, 0);
+	message[n] = '\0';
+        fprintf(stdout, "Received:\n%s\n", message);
+	for (int i = 0; i < n; ++i) message[i] = toupper(message[i]);
+	size_t sizeOfHtml = sizeof(htmlCode);
+	send(connfd, htmlCode, strlen(htmlCode), 0);
+	// Close the connection.
+    //	shutdown(connfd, SHUT_RDWR);
+       // close(connfd);
+	printf("end of for loop\n");
+    }
+    // Close the connection
+    shutdown(connfd, SHUT_RDWR);
+    close(connfd);
+    
+    printf("end of file\n");
+    return 0;
 }
